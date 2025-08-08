600c6c9f1c704b6d70d1479b138905409640e57e
cryptomafiaPB <jugad360@gmail.com>
2025-08-08 16:38:30 +0530

    Added a global error handler middleware and request body, params and query validator middleware.

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,1 +1,41 @@
- 
+import { NextFunction, Request, Response } from 'express';
+import ApiError from '../utils/api-error';
+import { ZodError } from 'zod';
+import { env } from '../utils/env';
+
+export default function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+
+    // zod validation error
+    if (err instanceof ZodError) {
+        return res.status(400).json({
+            statusCode: 400,
+            message: "Validation Error",
+            success: false,
+            status: "error",
+            details: err.issues,
+        });
+    }
+
+
+    // custom ApiError
+    if (err instanceof ApiError) {
+        return res.status(err.statusCode).json({
+            statusCode: err.statusCode,
+            message: err.message,
+            success: false,
+            status: "error",
+            details: err.error || undefined,
+        });
+    }
+
+    const message = err.message || 'Internal Server Error';
+
+    // Fallback for all other errors
+    return res.status(500).json({
+        statusCode: 500,
+        message: message,
+        success: false,
+        status: "error",
+        details: env.ENVIRONMENT === "development" ? err.stack : undefined,
+    });
+}
---
+++ server/src/middlewares/validateRequest.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import { ZodObject } from 'zod';
+
+// validate request body, params and query
+export const validateRequest = (schema: ZodObject) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+        schema.parse({
+            body: req.body,
+            params: req.params,
+            query: req.query,
+        });
+        next();
+    } catch (err) {
+        const error = err as any;
+        next(error);
+    }
+};
